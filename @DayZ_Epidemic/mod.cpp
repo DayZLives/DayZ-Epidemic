8f34fc47d1ceb1575c2e74fc875b7f74ab681d88
Krixes <crazyone_kay@hotmail.com>
2013-06-25 17:39:14 -0400

    FirstRelease1
    Release of first set of files.

---
+++ @DayZ_Epidemic/mod.cpp
@@ -0,0 +1,5 @@
+name = "DayZ Epidemic: Next Generation DayZ";
+picture = "z\addons\dayz_code\gui\EpidemicLogo.paa";
+actionName = "Website";
+action = "http://biodefenseforce.enjin.com";
+description = "";
