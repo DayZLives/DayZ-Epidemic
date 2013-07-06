d1df2433c52304b059ab187778154c17b3974522
Krixes <crazyone_kay@hotmail.com>
2013-07-06 17:15:16 -0400

    LeavingOnlyLauncher
    This commit is to leave only the launcher on GitHub. Files have gotten
    to large and am moving to another service.

--- @DayZ_Epidemic/mod.cpp
+++
@@ -1,5 +0,0 @@
-name = "DayZ Epidemic: Next Generation DayZ";
-picture = "\dayz_epidemic\textures\ui\EpidemicLogo.paa";
-actionName = "Website";
-action = "http://biodefenseforce.enjin.com";
-description = "";
